f6496ad03b62e76bdf75a463c1a6b5b9a98a853d
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-07-22 17:19:46 -0300

    Add Pokemon Context

--- src/Stores/Pokemon.context.tsx
+++ src/Stores/Pokemon.context.tsx
@@ -1,17 +1,44 @@
 import { ReactNode, createContext, useState } from "react";
 import { Pokemon } from "../Schemas";
+import { PokemonList } from "../Schemas/PokemonList";
 
 export type PokemonContextType = {
   pokemons: Pokemon[];
   setPokemons: React.Dispatch<React.SetStateAction<Pokemon[]>>;
+  selectedPokemon?: Pokemon;
+  setSelectedPokemon: React.Dispatch<React.SetStateAction<Pokemon | undefined>>;
+  pokemonList?: PokemonList[];
+  setPokemonList: React.Dispatch<React.SetStateAction<PokemonList[] | undefined>>;
+  selectPokemon: (id: number) => void;
 };
 
 export const PokemonContext = createContext<PokemonContextType | null>(null);
 
-export function AlquileresContextProvider({ children }: { children: ReactNode }) {
+export function PokemonContextProvider({ children }: { children: ReactNode }) {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [selectedPokemon, setSelectedPokemon] = useState<Pokemon>();
+  const [pokemonList, setPokemonList] = useState<PokemonList[]>();
+
+  function selectPokemon(id: number) {
+    const selected = pokemons.find((p) => p.id === id);
+    if (selected) {
+      setSelectedPokemon(selected);
+    }
+  }
 
   return (
-    <PokemonContext.Provider value={{ pokemons, setPokemons }}>{children}</PokemonContext.Provider>
+    <PokemonContext.Provider
+      value={{
+        pokemons,
+        setPokemons,
+        selectedPokemon,
+        setSelectedPokemon,
+        selectPokemon,
+        pokemonList,
+        setPokemonList,
+      }}
+    >
+      {children}
+    </PokemonContext.Provider>
   );
 }
