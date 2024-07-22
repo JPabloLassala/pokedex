699a2d7987103e5936c25fde4a76b38121eb58db
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-07-22 17:21:27 -0300

    Add context to main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { RootLayout } from "./Shared/RootLayout";
 import { PokemonPage } from "./Pokemon";
+import { PokemonContextProvider } from "./Stores";
 
 const router = createBrowserRouter([
   {
     path: "/pokedex",
     element: <RootLayout />,
     children: [
-      { index: true, element: <PokemonPage /> },
+      {
+        index: true,
+        element: (
+          <PokemonContextProvider>
+            <PokemonPage />
+          </PokemonContextProvider>
+        ),
+      },
       { path: "pokemon", element: <PokemonPage /> },
       { path: "items", element: <p>items</p> },
     ],
