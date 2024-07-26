ad5bb1b8c05b1b62e01644910c7edf3cfca224ab
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-07-26 15:09:19 -0300

    Split placeholders

--- src/Pokemon/PokemonListPlaceholder.tsx
+++ src/Pokemon/PokemonListPlaceholder.tsx
@@ -1,7 +1,6 @@
 export function PokemonListPlaceholder() {
   return (
     <main className="blur-sm transition-opacity flex flex-col w-full items-center gap-6">
-      <div className="animate-pulse bg-slate-300 rounded-2xl h-14 w-full"></div>
       <div className="animate-pulse flex flex-row gap-4 w-full">
         <div className="rounded-2xl bg-slate-300 w-1/4 h-52 mt-6"></div>
         <div className="rounded-2xl bg-slate-300 w-1/4 h-52 mt-6"></div>
---
+++ src/Pokemon/PokemonSearchPlaceHolder.tsx
@@ -0,0 +1,3 @@
+export function PokemonSearchPlaceholder() {
+  return <div className="animate-pulse bg-slate-300 rounded-2xl h-14 w-full"></div>;
+}
