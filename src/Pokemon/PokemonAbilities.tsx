da649a2fe85ff5c4e185894b860d1c62a713fa69
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-07-26 15:08:55 -0300

    Make abilities to in col direction

--- src/Pokemon/PokemonAbilities.tsx
+++ src/Pokemon/PokemonAbilities.tsx
@@ -18,7 +18,7 @@ export function PokemonAbilities({ pokemon }: { pokemon: Pokemon }) {
             textColor="text-slate-800"
             className={`px-6 py-2 break-keep uppercase border rounded-full min-w-36 flex flex-row justify-between items-center ${borderClass}`}
           >
-            <p className="text-xs">{ability.ability.name}</p>
+            <p className="text-[calc((1vw_+_1vh)*0.40)]">{ability.ability.name}</p>
             {isHidden && (
               <div>
                 <FontAwesomeIcon icon={faEyeSlash} className="ml-1" />
